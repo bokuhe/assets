1b8da52b0d96f26695f8ef35aac95055f6b90d65
insanity <bokuhe@gmail.com>
2022-11-19 13:37:34 +0900

    add: rc repo

---
+++ dev/react-native/rn-conf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ESLINT
+curl -o .eslintignore https://raw.githubusercontent.com/bokuhe/assets/main/dev/react-native/.eslintignore
+curl -o .eslintrc.js https://raw.githubusercontent.com/bokuhe/assets/main/dev/react-native/.eslintrc.js
+
+# PRETTIER
+curl -o .prettierignore https://raw.githubusercontent.com/bokuhe/assets/main/dev/react-native/.prettierignore
+curl -o .prettierrc.js https://raw.githubusercontent.com/bokuhe/assets/main/dev/react-native/.prettierrc.js
+
+# GITIGNORE
+curl -o .gitignore https://raw.githubusercontent.com/bokuhe/assets/main/dev/react-native/.gitignore
+
+# App.tsx
+curl -o App.tsx https://raw.githubusercontent.com/bokuhe/assets/main/dev/react-native/App.tsx
